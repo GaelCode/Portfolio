6aff668d0dbe7ee993c3c4221ae08e8deb7d944c
GaelCode <gaeloddosmarcel@gmail.com>
2025-01-18 21:29:22 +0100

    changement arrière

--- src/accueil/accueil.tsx
+++ src/accueil/accueil.tsx
@@ -23,13 +23,13 @@ const Accueil = () => {
                     <Header></Header>
                     <section className="accueil">
                         <div className="info animatable">
-                            <div className="hello">Hi, I'm</div>
-                            <div className="name">Gaël</div>
-                            <div className="learning">Computer Science student</div>
-                            <div className="position">From France</div>
+                        <div className="hello">Bonjour, je suis</div>
+                        <div className="name">Gaël</div>
+                        <div className="learning">Étudiant en informatique</div>
+                        <div className="position">français</div>
                             <div className="list-cta">
-                                <a href="/about"><Special_CTA texte="About"></Special_CTA></a>
-                                <a href="/projects"><CTA texte="Projects"></CTA></a>
+                                <a href="/about"><Special_CTA texte="À propos"></Special_CTA></a>
+                                <a href="/projects"><CTA texte="Projets"></CTA></a>
                             </div>
                         </div>
                         <div className="image-container animatableReverse">
--- src/fond/fond.tsx
+++ src/fond/fond.tsx
@@ -5,42 +5,38 @@ const Fond: React.FC = () => {
     const [largeur, setLargeur] = useState<number>(window.innerWidth);
     const [hauteur, setHauteur] = useState<number>(window.innerHeight);
     const [mousePosition, setMousePosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
-    // @ts-ignore
-    const [clicks, setClicks] = useState<Array<{ x: number; y: number; time: number }>>([]); // Liste des clics et leur temps
-    // @ts-ignore
-    const [pixelAnimationState, setPixelAnimationState] = useState<Map<string, { active: boolean; time: number }>>(new Map()); // Suivi des animations des pixels
+    const [pixelAnimationState, setPixelAnimationState] = useState<Map<string, { active: boolean; time: number }>>(new Map());
+    const [isMouseFollowEnabled, setIsMouseFollowEnabled] = useState<boolean>(window.innerWidth >= 1280); // state to enable/disable mouse follow
 
     useEffect(() => {
         const handleResize = () => {
             setLargeur(window.innerWidth);
             setHauteur(window.innerHeight);
+            // Disable mouse follow animation if window width is below 1280px
+            setIsMouseFollowEnabled(window.innerWidth >= 1200);
         };
 
-        const handleMouseMove = (e: MouseEvent) => {
-            setMousePosition({ x: e.clientX, y: e.clientY });
+        let animationFrameId: number;
+        const updateMousePosition = (e: MouseEvent) => {
+            if (!isMouseFollowEnabled) return; // Don't update mouse position if not enabled
+
+            cancelAnimationFrame(animationFrameId);
+            animationFrameId = requestAnimationFrame(() => {
+                setMousePosition({ x: e.clientX, y: e.clientY });
+            });
         };
 
         const handleMouseClick = (e: MouseEvent) => {
             const newClick = { x: e.clientX, y: e.clientY };
-            setClicks((prevClicks) => [
-                ...prevClicks,
-                { ...newClick, time: Date.now() }
-            ]);
 
-            // Définir la taille de la zone d'impact circulaire
-            const radius = 60; // Rayon de la zone d'impact circulaire
+            let radius = 96;
             const newPixelAnimationState = new Map(pixelAnimationState);
 
-            // Identifier la zone d'impact circulaire autour du clic
             for (let row = 0; row < rows; row++) {
                 for (let col = 0; col < cols; col++) {
                     const pixelX = col * squareSize + squareSize / 2;
                     const pixelY = row * squareSize + squareSize / 2;
-
-                    // Calculer la distance entre le clic et le pixel
                     const distance = Math.sqrt(Math.pow(newClick.x - pixelX, 2) + Math.pow(newClick.y - pixelY, 2));
-
-                    // Si la distance entre le clic et le pixel est inférieure au rayon, l'animation est activée
                     if (distance < radius) {
                         const key = `${col}-${row}`;
                         newPixelAnimationState.set(key, { active: true, time: Date.now() });
@@ -50,7 +46,6 @@ const Fond: React.FC = () => {
 
             setPixelAnimationState(newPixelAnimationState);
 
-            // Désactiver l'animation après 600ms
             setTimeout(() => {
                 const updatedState = new Map(pixelAnimationState);
                 newPixelAnimationState.forEach((value, key) => {
@@ -59,37 +54,43 @@ const Fond: React.FC = () => {
                     }
                 });
                 setPixelAnimationState(updatedState);
-            }, 600); // Durée de l'animation ripple (600ms)
+            }, 600);
         };
 
         window.addEventListener("resize", handleResize);
-        window.addEventListener("mousemove", handleMouseMove);
+        window.addEventListener("mousemove", updateMousePosition);
         window.addEventListener("click", handleMouseClick);
 
         return () => {
             window.removeEventListener("resize", handleResize);
-            window.removeEventListener("mousemove", handleMouseMove);
+            window.removeEventListener("mousemove", updateMousePosition);
             window.removeEventListener("click", handleMouseClick);
+            cancelAnimationFrame(animationFrameId);
         };
-    }, [pixelAnimationState]);
+    }, [pixelAnimationState, isMouseFollowEnabled]); // Re-run when isMouseFollowEnabled changes
 
-    const squareSize = 25;
-    const cols = Math.floor(largeur / squareSize);
-    const rows = Math.floor(hauteur / squareSize);
+    let cols = 30;
+    if (window.innerWidth < 1200 && window.innerWidth > 700) {
+        cols = 20;
+    } else if (window.innerWidth < 700) {
+        cols = 15;
+    }
+    const squareSize = largeur / cols;
+    const rows = Math.ceil(hauteur / squareSize);
 
-    // Fonction pour vérifier si un pixel a été cliqué récemment et a une animation
     const wasClicked = (x: number, y: number) => {
         const key = `${x}-${y}`;
         return pixelAnimationState.has(key) && pixelAnimationState.get(key)?.active;
     };
 
-    // Fonction pour calculer l'échelle de suivi de la souris
     const calculateMouseScale = (x: number, y: number) => {
+        if (!isMouseFollowEnabled) return 1; // No scaling if mouse follow is disabled
+
         const centerX = mousePosition.x;
         const centerY = mousePosition.y;
-        const distance = Math.sqrt(Math.pow(centerX - (x * squareSize + squareSize / 2), 2) + Math.pow(centerY - (y * squareSize + squareSize / 2), 2));
-        const scale = Math.max(1, 1 + (2 - distance / 100) * 1.5); // Augmenter la portée de l'animation de la souris
-        return scale;
+        let distance = Math.sqrt(Math.pow(centerX - (x * squareSize + squareSize / 2), 2) + Math.pow(centerY - (y * squareSize + squareSize / 2), 2));
+        
+        return Math.max(1, 1 + (1.5 - distance / 250) * 2);
     };
 
     return (
@@ -97,26 +98,16 @@ const Fond: React.FC = () => {
             {Array.from({ length: rows }).map((_, rowIndex) => (
                 <div className="row" key={rowIndex}>
                     {Array.from({ length: cols }).map((_, colIndex) => {
-                        // Vérifier si le pixel a été affecté par un clic
                         const clicked = wasClicked(colIndex, rowIndex);
                         const key = `${colIndex}-${rowIndex}`;
-
-                        // Vérifier si l'animation doit être en cours ou non
-                        pixelAnimationState.get(key);
-
-                        // Calculer l'effet de suivi de la souris
                         const mouseScale = calculateMouseScale(colIndex, rowIndex);
-
-                        // Appliquer l'animation du clic ou le suivi de la souris
                         const scale = clicked ? "ripple-animation" : "";
 
                         return (
-                            <div key={`${rowIndex}-${colIndex}`} className="square">
+                            <div key={key} className="square" style={{ width: squareSize, height: squareSize }}>
                                 <div
                                     className={`pixel ${scale}`}
-                                    style={{
-                                        transform: `scale(${mouseScale})`, // Appliquer le scale du suivi de la souris
-                                    }}
+                                    style={{ transform: `scale(${mouseScale})` }}
                                 ></div>
                             </div>
                         );
